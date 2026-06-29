7c6325130bf5f2960c45d1b92b5e640a552df2ac
Brandon Comer <comerb@umich.edu>
2026-06-29 18:37:19 -0400

    Apple updates, snake movement impl, bug fixes

--- board.h
+++ board.h
@@ -6,6 +6,12 @@
 
 using namespace std;
 
+struct PairHash {
+    size_t operator()(const std::pair<size_t, size_t>& p) const {
+        return std::hash<size_t>()(p.first) * std::hash<size_t>()(p.second);
+    }
+};
+
 class Snake {
     public:
         size_t size() {
@@ -17,31 +23,47 @@ class Snake {
             snake1.pop();
         };
 
-        void add(size_t x, size_t y) {
-            snake1.push({x, y});
-            snake2.insert({x, y});
+        void add(std::pair<size_t, size_t> next) {
+            snake1.push(next);
+            snake2.insert(next);
         };
 
         bool find(size_t x, size_t y) {
-            return (snake2.find({x, y}) == snake2.end());
+            return (snake2.find({x, y}) != snake2.end());
+        }
+
+        std::pair<size_t, size_t> head() {
+            return snake1.back();
         }
 
         std::queue<std::pair<size_t, size_t>> snake1;
-        std::unordered_set<std::pair<size_t, size_t>> snake2;
+        std::unordered_set<std::pair<size_t, size_t>, PairHash> snake2;
 };
 
 class Board {
     public:
-        Board(size_t board_size) {
+        Board(size_t board_size) : board(board_size, vector<bool>(board_size, false)), alive(true) {
             generateApple();
-
-            alive = true;
         }
 
         std::vector<std::vector<bool>> board;
         Snake snake;
         std::pair<size_t, size_t> apple;
         bool alive;
+
+        std::pair<size_t, size_t> addCoord(std::pair<size_t, size_t> a, std::pair<int, int> b) {
+            int x = static_cast<int>(a.first) + b.first;
+            int y = static_cast<int>(a.second) + b.second;
+
+            if (x < 0 || y < 0 ||
+                x >= static_cast<int>(board.size()) ||
+                y >= static_cast<int>(board.size()))
+                return a;
+
+            return {static_cast<size_t>(x),
+                    static_cast<size_t>(y)};
+        }
+
         bool complete() {
             return (board.size() * board[0].size() == snake.size());
         }
@@ -50,7 +72,7 @@ class Board {
             for (size_t i = 0; i < board.size() - 1; ++i) {
                 std::cout << board[i][0];
                 for (size_t j = 1; j < board[i].size(); ++j) {
-                    std::cout << board[i][j];
+                    std::cout << '|' << board[i][j];
                 }
                 for (size_t k = 0; k < board[i].size() * 2; ++k) {
                     std::cout << '-';
@@ -58,27 +80,36 @@ class Board {
             }
             std::cout << board[board.size() - 1][0];
             for (size_t j = 1; j < board[board.size() - 1].size(); ++j) {
-                std::cout << board[board.size() - 1][j];
+                std::cout << '|' << board[board.size() - 1][j];
+            }
+        };
+
+        void moveSnake(std::pair<int, int> move) {
+            std::pair<size_t, size_t> next = addCoord(snake.head(), move);
+            if (next == snake.head()) alive = false;
+            else {
+                if (next == apple) {
+                    snake.add(next);
+                    if (!complete()) generateApple();
+                }
+                else {
+                    snake.add(next);
+                    snake.pop();
+                }
             }
         };
 
         void generateApple() {
             std::random_device rd;
             std::mt19937 gen(rd());
-            std::uniform_int_distribution<> distr(0, board.size() - 1); 
+            std::uniform_int_distribution<> distr(0, static_cast<int>(board.size() - 1)); 
             size_t x = static_cast<size_t>(distr(gen));
             size_t y = static_cast<size_t>(distr(gen));
             while (snake.find(x, y)) {
-                if (snake.find((x + 1) % board.size(), y)) {
-                    apple = {(x + 1) % board.size(), y};
-                }
-                else if (snake.find(x, (y + 1) % board.size())) {
-                    apple = {x, (y + 1) % board.size()};
-                }
-                else {
-                    x = (x + 1) % board.size();
-                    y = (y + 1) % board.size();
-                }
+                //if the random apple lands on a snake tile, linear probe to a new spot
+                x = (x + 1) % board.size();
+                if (x == 0) y = (y + 1) % board.size();
             }
+            apple = {x, y};
         }
 };
--- snake.cpp
+++ snake.cpp
@@ -8,18 +8,16 @@ std::unordered_map<char, std::pair<int, int>> moves = {
     {'R', {1, 0}}
 };
 
-bool validAction(char action) {
-    return (action == 'U' || action == 'L' || action == 'D' || action == 'R');
-};
-
 using namespace std;
 
 int main() {
-    size_t board_size;
+    size_t board_size = 0;
     std::cout << "Welcome to Snake..." << '\n';
     std::cout << "How big would you like the square to be? (Enter a value 2-12): ";
-    std::cin >> board_size;
-    std::cout << '\n';
+    while (board_size < 2 || board_size > 20) {
+        std::cin >> board_size;
+        std::cout << '\n';
+    }
 
     Board board(board_size);
 
@@ -29,12 +27,13 @@ int main() {
         std::cout << "Move: ";
         std::cin >> action;
         std::cout << '\n';
-        while (!validAction(action)) {
+        while (moves.find(action) == moves.end()) {
             std::cout << "Invalid move, try again: ";
             std::cin >> action;
             std::cout << '\n';
         }
+        board.moveSnake(moves[action]);
     }
     
-    return;
+    return 0;
 }
